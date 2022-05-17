8582608defb93dc99ea3bdf8894f3c297d70fdda
ayinlaabel <kraneabel@gmail.com>
2022-05-17 21:34:04 +0100

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,4 @@ import { SwPush } from "@angular/service-worker";
 })
 export class AppComponent {
   title = "taskmanager";
-
-  constructor(private swPush: SwPush) {}
-  ngOnInit() {
-    this.pushNotification();
-  }
-
-  async pushNotification() {
-    console.log("Registering service worker...");
-
-    const sub = await this.swPush.requestSubscription({
-      serverPublicKey:
-        "BJ7LRDAPf5UTP5x_HfdZlYYdSh1NbpTxIwBjPSe5k_11Zz9KVfkU6-5nG_AZm0I2BRqnHnAfqckJnitw2QUtmiw",
-    });
-  }
 }
