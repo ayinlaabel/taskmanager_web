5a7c5323cdad116eaba6a85bf9f9973f115dbaa1
ayinlaabel <kraneabel@gmail.com>
2022-05-20 06:39:20 +0100

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+
 app.use(express.static(__dirname + "/dist/taskmanager"));
 
 app.get("/*", function (req, res) {
